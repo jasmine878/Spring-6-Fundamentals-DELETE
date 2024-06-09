6ca3a5bef983af5e60862d3bedc6195e53fd90ed
Tae Hui Chang <jasmine878@yahoo.com>
2024-06-08 20:16:54 -0500

    Create a SpeakerService Bean

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,13 @@
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    @Bean
+    public SpeakerService getSpeakerService() {
+        return new SpeakerServiceImpl();
+    }
 }
