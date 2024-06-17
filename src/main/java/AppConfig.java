c0d0a75c1e5484c25e7d48e00b3fbbb835902843
Tae Hui Chang <jasmine878@yahoo.com>
2024-06-17 01:31:20 -0500

    Change the Bean Scope back to Singleton

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Scope;
 public class AppConfig {
 
     @Bean(name="speakerService")
-    @Scope(value= BeanDefinition.SCOPE_PROTOTYPE)
+    @Scope(value= BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
 //        service.setRepository(getSpeakerRepository());
