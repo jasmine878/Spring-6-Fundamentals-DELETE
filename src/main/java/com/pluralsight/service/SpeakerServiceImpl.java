b02232073e950d4069c292e2ffc65b7c1cffe847
Tae Hui Chang <jasmine878@yahoo.com>
2024-06-18 23:46:48 -0500

    Add Autowire annotation

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -2,22 +2,30 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl repository constructor");
+        this.repository = repository;
+    }
+
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakerRepository repository) {
-        this.repository = repository;
-    }
-
-    public SpeakerServiceImpl(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
     }
 }
