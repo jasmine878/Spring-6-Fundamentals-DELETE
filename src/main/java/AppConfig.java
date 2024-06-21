b85a9df358ce7243e0705e955ba59821e47e140a
Tae Hui Chang <jasmine878@yahoo.com>
2024-06-21 00:20:28 -0500

    Replace Bean with service stereotype annotation

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,15 +1,14 @@
 import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.repository.StubSpeakerRepositoryImpl;
-import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
+    /*
     @Bean(name="speakerService")
     @Scope(value= BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
@@ -18,7 +17,7 @@ public class AppConfig {
 //        service.setRepository(getSpeakerRepository());
 
         return service;
-    }
+    }*/
 
     @Bean(name="speakerRepository")
     public SpeakerRepository getSpeakerRepository() {
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,9 +3,14 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
+@Scope(value= BeanDefinition.SCOPE_SINGLETON)
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
 
